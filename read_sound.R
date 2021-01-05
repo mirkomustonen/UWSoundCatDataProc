05a28896dcd72a12306ca23a403d366157bb8949
Mirko Mustonen <mirkomustonen@gmail.com>
2021-01-05 16:37:23 +0200

    Completed writing the read_sound.R function, added functionality of reading sound only from certain time period.

--- read_sound.R
+++ read_sound.R
@@ -1,121 +1,48 @@
-library(rhdf5)
-
-loc <- 'B23'
-
-
-path <- "PAM_Output"
-loc_files <- list.files(path = path, pattern = paste0('+', loc, '+'))
-
-
-TOBs <- read.table(file = 'Third_octs.txt', sep = '\t', header = TRUE)
-
-k <- 0
-for (i_file in loc_files) {
-        file_path <- file.path(path, i_file)
-        TOB_data_path <- "/Data/LeqMeasurementsOfChannel1"
-        DateTime_data_path <- "/Data/DateTime"
-        Freqs_data_path <- "Metadata/FrequencyIndex"
-        
-        TOB_data <- h5read(file_path, TOB_data_path)
-        DateTime_data <- h5read(file_path, DateTime_data_path)
-        Freqs_data <- h5read(file_path, Freqs_data_path)
-        
-        DateTime <- as.POSIXct(DateTime_data, tz = 'UTC')
-        
-        min_tob <- which.min(abs(TOBs$Base_10_Calculated_Frequency - Freqs_data[1]))
-        
-        Freqs_nom <- TOBs$Nominal_Frequency[min_tob:(min_tob - 1 + length(Freqs_data))]
-        
-        TOB_data <- as.data.frame(t(TOB_data))
-        names(TOB_data) <- as.character(Freqs_data)
-        TOB_data$DateTime <- DateTime
-        if (k == 0) {
-                TOB_data_l <- TOB_data
-                k <- 1
-        }
-        else{
-                TOB_data_l <- rbind(TOB_data_l, TOB_data)
-        }    
-
+read_sound <- function(loc, beg_period, end_period) {
+  
+  library(rhdf5)  # Library for handling HDF5 files
+  library(lubridate)  # Library for better datetime handling
+  
+  beg_period <- as.POSIXct(beg_period, format =  '%Y-%m-%d', tz = 'UTC')  # Convert the input beginning time period to POSIXct time format
+  end_period <- as.POSIXct(end_period, format =  '%Y-%m-%d', tz = 'UTC')  # Convert the input end time period to POSIXct time format
+  sel_period <- interval(beg_period, end_period, tzone = "UTC")  # Create a time period from the selected beginning and end times
+  
+  loc_files <- list.files(path = "PAM_Output", pattern = paste0('+', loc, '+'))  # List the HDF5 files to be read in
+  
+  TOBs <- read.table(file = 'Third_octs.txt', sep = '\t', header = TRUE)  # Read third-octave nominal and numerical values from file 
+  
+  # Loop through the HDF5 SPL data files and read the data into a dataframe
+  k <- 0  # Flag value for marking the first iteration
+  for (i_file in loc_files) {
+          f_beg_d <- as.POSIXct(substr(i_file, 26, 33), format =  '%Y%m%d', tz = 'UTC')  # Beginning time of the file to be read in
+          f_end_d <- as.POSIXct(substr(i_file, 35, 42), format =  '%Y%m%d', tz = 'UTC')  # End time of the file to be read in
+          file_period <- interval(f_beg_d, f_end_d, tzone = "UTC") # Create a time period with file beginning and end times
+          
+          file_per_test <- !is.na(intersect(sel_period, file_period))  # Check whether there is overlap between the selected and file time periods
+          if (file_per_test) {
+            file_path <- file.path("PAM_Output", i_file)  # Construct the HDF5 file path to be read in
+            
+            TOB_data <- h5read(file_path, "/Data/LeqMeasurementsOfChannel1")  # Read Third Octave Band data from the file in matrix form
+            TOB_data <- as.data.frame(t(TOB_data))  # Transform the TOB data matrix and convert to dataframe
+            
+            Freqs_data <- h5read(file_path, "Metadata/FrequencyIndex")  # Read the center frequencies of the third octave bands
+            min_tob <- which.min(abs(TOBs$Base_10_Calculated_Frequency - Freqs_data[1]))  # For first TOB center freq find index of closest value from the nominal values list
+            Freqs_nom <- TOBs$Nominal_Frequency[min_tob:(min_tob - 1 + length(Freqs_data))]  # Extract the nominal TOB values from the TOBs dataframe
+            names(TOB_data) <- as.character(Freqs_nom)  # Assign the nominal TOB values as dataframe column names
+            
+            DateTime_data <- h5read(file_path, "/Data/DateTime")  # Read the TOB data time stamps from the HDF5 file
+            TOB_data$DateTime <- as.POSIXct(DateTime_data, tz = 'UTC')  # Convert the time stamps to POSIXct format and add as dataframe column vector
+            
+            if (k == 0) {
+                    TOB_data_l <- TOB_data  # For first iteration create new table the data will be added to
+                    k <- 1  # Set the first iteration flag to 1
+            }
+            else{
+                    TOB_data_l <- rbind(TOB_data_l, TOB_data)  # For iterations after first aggregate dataframes into larger dataframe
+            }
+          }
+  }
+  TOB_data_l <- subset(TOB_data_l, DateTime > beg_period)  # Subset data to selected time period
+  TOB_data_l <- subset(TOB_data_l, DateTime < end_period)  # Subset data to selected time period
+  return(list(TOB_data_l, Freqs_data))
 }
-
-## Correlation plot
-
-remove(Freqs_data, TOB_data, DateTime, DateTime_data, DateTime_data_path, 
-       file_path, Freqs_data_path, i_file, k, loc_files, path, TOB_data_path,
-       TOBs)
-
-TOB_data_l <- subset(TOB_data_l, DateTime < as.POSIXct('2015-01-01', format =  '%Y-%m-%d', tz = 'UTC'))
-
-TOB_data_l <- subset(TOB_data_l, DateTime > as.POSIXct('2014-07-01', format =  '%Y-%m-%d', tz = 'UTC'))
-
-TOB_data_l <- subset(TOB_data_l, DateTime < as.POSIXct('2014-08-02', format =  '%Y-%m-%d', tz = 'UTC'))
-
-# sound.cor = cor(TOB_data_l[  , 1:(ncol(TOB_data_l) - 1)], method = c("spearman"))
-# 
-# library(corrplot)
-# png(file = paste0(loc, "corr_plot2.png"))
-# corrplot(sound.cor, is.corr = FALSE, cl.lim = c(0,1))
-# title(loc)
-# dev.off()
-
-
-## Sprectra density plot
-
-library(data.table)
-
-TOB_melt <- melt(TOB_data_l[,1:(ncol(TOB_data_l) - 1)], value.name = 'SPL')
-
-# for (third_oct in levels(TOB_melt$variable)) {
-#         TOB_melt_TOB <- subset(TOB_melt, variable == third_oct)
-# 
-#         TOB_hist <- hist(TOB_melt_TOB$SPL, breaks = 100)
-#         max_dif <- sort(TOB_hist$counts, decreasing = TRUE)[1] - sort(TOB_hist$counts, decreasing = TRUE)[2]
-#         if (max_dif > 500) {
-#                 print(third_oct)
-#                 self_n_l <- TOB_hist$breaks[which.max(a$counts)]
-#                 TOB_melt <- subset(TOB_melt, !(variable == third_oct & SPL <= (self_n_l + 1)))
-#         }
-# 
-# }
-
-TOB_melt$variable <- as.numeric(as.character(TOB_melt$variable))
-
-
-
-
-
-library(RColorBrewer)
-rf <- colorRampPalette(rev(brewer.pal(11,'Spectral')))
-r <- rf(42)
-
-f_c <- as.numeric(names(TOB_data_l)[1:(length(names(TOB_data_l)) - 1)])
-f_u <- f_c*10^0.05
-f_l <- f_c/10^0.05
-
-x.bin <- c(f_l, f_u[length(f_u)])
-
-SPL_min <- 55
-SPL_max <- 155
-SPL_bin <- (SPL_max - SPL_min)
-y.bin <- seq(SPL_min, SPL_max, length = SPL_bin)
-
-freq <-  as.data.frame(table(findInterval(TOB_melt$variable, x.bin),findInterval(TOB_melt$SPL, y.bin)))
-freq[,1] <- as.numeric(freq[,1])
-freq[,2] <- as.numeric(freq[,2])
-freq2D <- diag(ncol = SPL_bin, nrow =  length(x.bin))*0
-freq2D[cbind(freq[,1], freq[,2])] <- freq[,3]
-
-freq2D[freq2D == 0] <- NA 
-
-p_title <- paste(loc, substr(as.character(min(TOB_data_l$DateTime)), 1, 10), ' ', substr(as.character(max(TOB_data_l$DateTime)), 1, 10))
-png(file = paste0(p_title, ".png"), width = 1000, height = 600, res = 100)
-image(x.bin, y.bin, freq2D, col = r, 
-      xlab = "Frequency [Hz]", 
-      ylab = "SPL [dB re 1 uPa]", main = p_title,
-      xlim = c(min(f_l), max(f_l)),
-      xaxt = "n",
-      log = "x")
-axis(1, at = f_l, labels = as.character(Freqs_nom))
-
-dev.off()
